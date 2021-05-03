ad305c5c4033206c7f8f034ca470a3ce7b14935d
filipecamargos <filipe.atrabalho@gmail.com>
2021-05-03 14:12:11 -0600

    Change on the app.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment, useEffect } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  //UseEffect to avoid no related and infite loops
+  //Takes a anounimus function and array of dependencies
+  useEffect(() => {
+    const storedLoggedIn = localStorage.getItem("isLoggedIn");
+
+    if (storedLoggedIn === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []) //Manage to handle for the first time since the dependency never changes
+
   const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
+    localStorage.setItem("isLoggedIn", "1");
     setIsLoggedIn(true);
   };
-
+  
   const logoutHandler = () => {
+    localStorage.removeItem(isLoggedIn);
     setIsLoggedIn(false);
   };
 
   return (
-    <React.Fragment>
+    <Fragment>
       <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
-    </React.Fragment>
+    </Fragment>
   );
 }
 
